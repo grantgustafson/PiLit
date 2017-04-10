648cb70992af3fdbdce49f5969c12865b178a08e
Grant Gustafson <grantgustafson@grantpro.devices.brown.edu>
2017-04-10 09:05:11 -0400

    changed client IFACE to eth0

--- src/client/status_service.py
+++ src/client/status_service.py
@@ -5,10 +5,10 @@ from net_info import get_hw_addr, get_ip_addr
 import json
 
 status_service = Blueprint('status_service', __name__)
-IFACE = 'wlan0'
+IFACE = 'eth0'
 
 @status_service.route('/status', methods=['GET'])
 def status():
     return jsonify({'active' : True,
-                    'MAC': 'temp mac',
-                    'ip' : 'temp ip'})
+                    'MAC': get_hw_addr(IFACE),
+                    'ip' : get_ip_addr(IFACE)})
